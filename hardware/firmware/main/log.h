b01bd506bcff846fd28b53cc612f7346ee3c01c5
riesinger <mail@pascal-riesinger.de>
2019-10-26 15:35:48 +0200

    Simplify Log class
    Declared two macros (PRINT and PRINTLN) for calling the Serial functions
    instead of checking for NDEBUG in every method. Still keeping the
    methods for type safety.
    Also converted the class to a struct, since all methods are public
    anyways.

--- hardware/firmware/main/log.h
+++ hardware/firmware/main/log.h
@@ -2,91 +2,41 @@
 
 #include "Arduino.h"
 
+#ifndef NDEBUG
+#define PRINTLN(x) Serial.println(x)
+#define PRINT(x) Serial.print(x)
+#else
+#define PRINTLN(x)
+#define PRINT(x)
+#endif
+
 /**
  * Abstraction around Serial for disabling Serial output when not in debug mode
  */
-class Log {
-  public:
-    static void initSerial(const unsigned long baudRate) {
-      #ifndef NDEBUG
-        Serial.begin(baudRate);
-      #endif
-    }
-    static void println(const Printable& value) {
-      #ifndef NDEBUG
-        Serial.println(value);
-      #endif
-    }
-    static void println(const char* value) {
-      #ifndef NDEBUG
-        Serial.println(value);
-      #endif
-    }
-    static void println(const char value) {
-      #ifndef NDEBUG
-        Serial.println(value);
-      #endif
-    }
-    static void println(const StringSumHelper& value) {
-      #ifndef NDEBUG
-        Serial.println(value);
-      #endif
-    }
-    static void println() {
-      #ifndef NDEBUG
-        Serial.println();
-      #endif
-    }
-    static void println(const int32_t value) {
-      #ifndef NDEBUG
-        Serial.println(value);
-      #endif
-    }
-    static void println(const uint32_t value) {
-      #ifndef NDEBUG
-        Serial.println(value);
-      #endif
-    }
-    static void println(const __FlashStringHelper* value) {
-      #ifndef NDEBUG
-        Serial.println(value);
-      #endif
-    }
+struct Log {
+  static void initSerial(const unsigned long baudRate) {
+    #ifndef NDEBUG
+      Serial.begin(baudRate);
+    #endif
+  }
 
-    static void print(const Printable& value) {
-      #ifndef NDEBUG
-        Serial.print(value);
-      #endif
-    }
-    static void print(const char* value) {
-      #ifndef NDEBUG
-        Serial.print(value);
-      #endif
-    }
-    static void print(const char value) {
-      #ifndef NDEBUG
-        Serial.print(value);
-      #endif
-    }
-    static void print(const StringSumHelper& value) {
-      #ifndef NDEBUG
-        Serial.print(value);
-      #endif
-    }
-    static void print(const int32_t value) {
-      #ifndef NDEBUG
-        Serial.print(value);
-      #endif
-    }
-    static void print(const uint32_t value) {
-      #ifndef NDEBUG
-        Serial.print(value);
-      #endif
-    }
-    static void print(const __FlashStringHelper* value) {
-      #ifndef NDEBUG
-        Serial.print(value);
-      #endif
-    }
+  static void println() { PRINTLN(); }
+  static void println(const Printable& value) { PRINTLN(value); }
+  static void println(const char* value) { PRINTLN(value); }
+  static void println(const char value) { PRINTLN(value); }
+  static void println(const StringSumHelper& value) { PRINTLN(value); }
+  static void println(const int32_t value) { PRINTLN(value); }
+  static void println(const uint32_t value) { PRINTLN(value); }
+  static void println(const __FlashStringHelper* value) { PRINTLN(value); }
+  static void println(const unsigned long value) { PRINTLN(value); }
+
+  static void print(const Printable& value) { PRINT(value); }
+  static void print(const char* value) { PRINT(value); }
+  static void print(const char value) { PRINT(value); }
+  static void print(const StringSumHelper& value) { PRINT(value); }
+  static void print(const int32_t value) { PRINT(value); }
+  static void print(const uint32_t value) { PRINT(value); }
+  static void print(const __FlashStringHelper* value) { PRINT(value); }
+  static void print(const unsigned long value) { PRINT(value); }
 
 };
