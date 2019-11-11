8bf0f1bab765d367f43a156419948f350a018da5
riesinger <mail@pascal-riesinger.de>
2019-11-11 22:27:27 +0100

    Start switching to PlatformIO for firmware

---
+++ hardware/firmware/src/main.cpp
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+
+#include "log.h"
+#include "scanResult.h"
+
+#include <ArduinoJson.h>
+
+#define RUNS 250
+
+void serializeData(size_t);
+
+void setup() {
+  initSerial(115400);
+  logln("Starting");
+  auto start = millis();
+  for (size_t i = 0; i < RUNS; i++) {
+    serializeData(i);
+  }
+  auto end = millis();
+  logln();
+  log("Serialized ");
+  log(RUNS);
+  log(" scan results in ");
+  log(end - start);
+  logln("ms");
+}
+
+void serializeData(size_t count) {
+  DynamicJsonDocument doc(JSON_OBJECT_SIZE(4));
+  ScanResult res{-count, "AA:BB:CC:DD:EE", "MyWifiNetwork"};
+  res.print();
+  doc['RSSI'] = res.RSSI;
+  doc['BSSID'] = res.BSSID;
+  doc['SSID'] = res.SSID;
+  serializeJson(doc, Serial);
+}
+
+void loop() { }
