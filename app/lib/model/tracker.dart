1eddf738d473a17aaecdf1cf43c37bb64c5781b1
Max Heidinger <mail@max-heidinger.de>
2019-11-19 10:25:51 +0100

    Use json annotation in flutter example

--- app/lib/model/tracker.dart
+++ app/lib/model/tracker.dart
@@ -1,17 +1,19 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'tracker.g.dart';
+
+@JsonSerializable()
 class Tracker {
+  @JsonKey(name: "ID")
   int id;
+  @JsonKey(name: "Label")
   String label;
+  @JsonKey(name: "LastPoll")
   String lastPoll;
+  @JsonKey(name: "LastSleepTime")
   String lastSleepTime;
 
   Tracker({this.id, this.label, this.lastPoll, this.lastSleepTime});
 
-  factory Tracker.fromJson(Map<String, dynamic> json) {
-    return Tracker(
-      id: json['ID'],
-      label: json['Label'],
-      lastPoll: json['LastPoll'],
-      lastSleepTime: json['LastSleepTime'],
-    );
-  }
+  factory Tracker.fromJson(Map<String, dynamic> json) => _$TrackerFromJson(json);
+  Map<String, dynamic> toJson() => _$TrackerToJson(this);
 }
