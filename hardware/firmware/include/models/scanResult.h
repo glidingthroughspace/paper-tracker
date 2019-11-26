533f4ca3597958968d9d40c2cac00ced62c81725
Pascal Riesinger <mail@pascal-riesinger.de>
2019-11-26 09:46:25 +0100

    Hardware: Move bssid_to_string to utils

--- hardware/firmware/include/models/scanResult.h
+++ hardware/firmware/include/models/scanResult.h
@@ -8,9 +8,6 @@
 namespace cbor = ::qindesign::cbor;
 
 #define SSID_LENGTH 32
-#define BSSID_LENGTH 6
-// Two characters per byte + colons between bytes + NULL terminator
-#define BSSID_STRING_LENGTH BSSID_LENGTH * 2 + BSSID_LENGTH
 
 #define SCAN_RESULT_SIZE_BYTES sizeof(ScanResult)
 
@@ -24,25 +21,10 @@ struct ScanResult {
     log(SSID);
     log(", BSSID: ");
     char buf[BSSID_STRING_LENGTH];
-    bssid_to_string(buf);
+    utils::bssid_to_string(BSSID, buf);
     log(buf);
     log(", RSSI: ");
     logln(RSSI);
-  }
-
-  /**
-   * Converts the BSSID to a C string. 
-   * @param buf An 18 bytes long buffer to fill with the BSSID. Use the BSSID_STRING_LENGTH constant
-   * to allocate your buffer.
-   */
-  void bssid_to_string(char* buf) {
-    // A  A  :  B  B  :  C  C  :  D  D  :  E  E  :  F  F
-    // 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16
-    buf[2] = buf[5] = buf[8] = buf[11] = buf[14] = ':';
-    for (int i = 0; i < BSSID_LENGTH; i++) {
-      utils::byte_to_hex(BSSID[i], &buf[i * 3]);
-    }
-    buf[17] = '\0';
   }
 
   bool toCBOR(cbor::Writer& cbor) {
