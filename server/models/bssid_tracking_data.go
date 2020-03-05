f9deefc1ed31befcc8a06ba22e1f2168b58011b1
riesinger <mail@pascal-riesinger.de>
2020-03-05 10:23:40 +0100

    Server: Cleanup BBSIDTrackingData DB interface

--- server/models/bssid_tracking_data.go
+++ server/models/bssid_tracking_data.go
@@ -1,19 +1,17 @@
 package models
 
-import "github.com/jinzhu/gorm"
-
 // BSSIDTrackingData is consolidated tracking data for a single BSSID
 // Note that all sizes (min, max, ...) are in their mathematical form, not the logical form:
 // a RSSI of -90dBm is worse than -10dBm. The Minimum in this case is -90dBm, as one might expect.
 type BSSIDTrackingData struct {
-	gorm.Model
+	ID        uint
 	RoomID    RoomID
-	BSSID     string    `json:"bssid"`
+	BSSID     string    `json:"bssid" gorm:"column:bssid"`
 	Minimum   int       `json:"minimum"`
 	Maximum   int       `json:"maximum"`
 	Median    float64   `json:"median"`
 	Mean      float64   `json:"mean"`
-	Quantiles Quantiles `json:"quartiles"`
+	Quantiles Quantiles `json:"quantiles" gorm:"embedded"`
 }
 
 type Quantiles struct {
