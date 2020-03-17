9e6f8a07231a2113a1c894c23f56cb5b75c44c13
riesinger <mail@pascal-riesinger.de>
2020-03-17 12:43:45 +0100

    Hardware: Implement request timeouts

--- hardware/firmware/src/apiClient.cpp
+++ hardware/firmware/src/apiClient.cpp
@@ -7,7 +7,7 @@
 
 #include <models/newIDResponse.hpp>
 
-std::map<uint16_t, coap_callback> ApiClient::callbacks;
+std::map<uint16_t, ApiClient::Callback> ApiClient::callbacks;
 
 ApiClient::ApiClient(WiFiUDP* udp, IPAddress serverIP)
   : coap(udp), serverIP(serverIP) {
@@ -20,13 +20,19 @@ bool ApiClient::start() {
 }
 
 bool ApiClient::loop() {
+  update_request_timeouts();
   return coap.loop();
 }
 
 void ApiClient::requestTrackerID(std::function<void(int16_t)> callback) {
   logln("Requesting new tracker ID from server");
   auto message_id = coap.post(serverIP, "tracker/new");
-  storeCallback(message_id, [callback] (coap::Packet& packet) {
+  storeCallback(message_id, [callback] (coap::Packet& packet, bool timed_out) {
+    if (timed_out) {
+      logln("Requesting a new tracker ID timed out. Cannot continue operation, going to sleep");
+      Power::deep_sleep_for_seconds(10);
+      return;
+    }
     if (ApiClient::isErrorResponse(packet)) {
       logln("Requesting a new tracker ID failed. Cannot continue operation, sleeping 10 seconds");
       Power::deep_sleep_for_seconds(10);
@@ -39,7 +45,7 @@ void ApiClient::requestTrackerID(std::function<void(int16_t)> callback) {
       return;
     }
     callback(nid.getID());
-  });
+  }, 20); // This is a one-time operation, so we can cope with a longer timeout to make it more likely that the action succeeds
 }
 
 void ApiClient::requestNextCommand(uint16_t trackerID, std::function<void(Command&)> callback) {
@@ -49,7 +55,12 @@ void ApiClient::requestNextCommand(uint16_t trackerID, std::function<void(Comman
   char param[15];
   auto qp = getTrackerIDQueryParam(param, 15, trackerID);
   uint16_t messageID = coap.get(serverIP, "tracker/poll", qp);
-  storeCallback(messageID, [callback] (coap::Packet& packet) {
+  storeCallback(messageID, [callback] (coap::Packet& packet, bool timed_out) {
+    if (timed_out) {
+      logln("Requesting the next action timed out, going to sleep");
+      Power::deep_sleep_for_seconds(10);
+      return;
+    }
     if (ApiClient::isErrorResponse(packet)) {
       logln("Requesting the next action failed, going to sleep for 10 seconds");
       Power::deep_sleep_for_seconds(10);
@@ -75,7 +86,11 @@ void ApiClient::writeTrackingData(uint16_t trackerID, std::vector<uint8_t> scanR
   char param[15];
   auto qp = getTrackerIDQueryParam(param, 15, trackerID);
   auto msgID = coap.post(serverIP, "tracker/tracking", qp, scanResults, ContentType::APPLICATION_CBOR);
-  storeCallback(msgID, [callback] (coap::Packet& packet) {
+  storeCallback(msgID, [callback] (coap::Packet& packet, bool timed_out) {
+    if (timed_out) {
+      logln("Request to send tracking data timed out, continuing");
+      callback();
+    }
     if (ApiClient::isErrorResponse(packet)) {
       logln("Failed to send tracking data");
       logln(packet.code);
@@ -101,17 +116,21 @@ void ApiClient::coap_response_callback(coap::Packet& packet, IPAddress ip, int p
     logln(packet.messageid);
     return;
   }
-  it->second(packet);
+  it->second.function(packet, false);
 }
 
-void ApiClient::storeCallback(uint16_t messageID, coap_callback callback) {
+void ApiClient::storeCallback(uint16_t messageID, coap_callback callback, utils::time::seconds timeout) {
   if (messageID == 0) {
     logln("Sending the message failed");
     return;
   }
   log("Message has ID ");
   logln(messageID);
-  callbacks[messageID] = callback;
+  callbacks[messageID] = Callback{
+    timeout,
+    utils::time::current(),
+    callback,
+  };
 }
 
 bool ApiClient::isErrorResponse(const coap::Packet& response) {
@@ -127,4 +146,17 @@ std::vector<char*> ApiClient::getTrackerIDQueryParam(char* buffer, size_t buffer
     logln(qp);
   }
   return queryParams;
+}
+
+void ApiClient::update_request_timeouts() {
+  for (auto& callback : callbacks) {
+    auto cb = callback.second;
+    if (utils::time::current_time_is_after(cb.request_started_at + utils::time::to_millis(cb.timeout))) {
+      log("Request with ID ");
+      log(callback.first);
+      logln(" timed out");
+      coap::Packet empty_packet;
+      cb.function(empty_packet, true);
+    }
+  }
 }
