df67aa06b01e5685439631f4a1271113ee25ccde
riesinger <mail@pascal-riesinger.de>
2020-04-02 12:11:53 +0200

    Hardware: Fix battery percentage calculation

--- hardware/firmware/src/power.cpp
+++ hardware/firmware/src/power.cpp
@@ -7,8 +7,9 @@ TinyPICO Power::tinypico = TinyPICO();
 // The battery voltage to consider as 0% charged. Anything lower than that will return negative (or
 // rolled over) battery percentages. This is taken from the battery's specification.
 constexpr float min_battery_voltage = 3.0;
-// Maximum voltage considered as 100% battery
-constexpr float max_battery_voltage = 4.25;
+// Maximum voltage considered as 100% battery. The PMIC typically stop charging at around 4.19. The
+// percentage will be clipped to 100 at that point.
+constexpr float max_battery_voltage = 4.18;
 
 void Power::print_wakeup_reason() {
   #ifndef NDEBUG
@@ -37,13 +38,19 @@ uint64_t Power::seconds_to_microseconds(const uint64_t seconds) {
 
 uint8_t Power::get_battery_percentage() {
   auto voltage = tinypico.GetBatteryVoltage();
-  uint8_t percentage = (uint8_t)((voltage - min_battery_voltage) / (max_battery_voltage - min_battery_voltage)) * 100;
-  if (percentage > 100) {
-    percentage = 0;
-  }
+  log("The battery voltage is: ");
+  log(voltage);
+  uint8_t percentage = (uint8_t)((voltage - min_battery_voltage) / (max_battery_voltage - min_battery_voltage) * 100);
+  // Clip to 100%
+  percentage = percentage < 100 ? percentage : 100;
+  log(", this equals a percentage of: ");
+  logln(percentage);
   return percentage;
 }
 
 bool Power::is_charging() {
-  return tinypico.IsChargingBattery();
+  auto charging = tinypico.IsChargingBattery();
+  log("We are charging: ");
+  logln(charging);
+  return charging;
 }
