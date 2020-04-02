8bd9d8efa83a0df5befe48ac44afb284946c566e
Max Heidinger <mail@max-heidinger.de>
2020-04-02 10:50:40 +0200

    Try inlining TrackerCmdResponse in TrackingCmdResponse

--- server/models/communication/tracker.go
+++ server/models/communication/tracker.go
@@ -3,15 +3,15 @@ package communication
 import "paper-tracker/models"
 
 type TrackerCmdResponse struct {
-	BatteryPercentage int  `json:"battery_percentage"`
-	IsCharging        bool `json:"is_charging"`
+	BatteryPercentage int  `codec:"battery_percentage"`
+	IsCharging        bool `codec:"is_charging"`
 }
 
 type TrackingCmdResponse struct {
-	TrackerCmdResponse `json:"tracker_cmd_response"`
+	TrackerCmdResponse
 	// ResultID is an ID for identifying batches comprising a single result
-	ResultID uint64 `json:"result_id"`
+	ResultID uint64 `codec:"result_id"`
 	// BatchCount batches are in the given result
-	BatchCount  uint8                `json:"result_batch_count"`
-	ScanResults []*models.ScanResult `json:"scan_results"`
+	BatchCount  uint8                `codec:"result_batch_count"`
+	ScanResults []*models.ScanResult `codec:"scan_results"`
 }
