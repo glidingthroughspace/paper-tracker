e188943e7d7a686fc3a53669bde3a061801818e5
riesinger <mail@pascal-riesinger.de>
2020-04-03 15:40:07 +0200

    Server: Add comments to tracker model

--- server/models/tracker.go
+++ server/models/tracker.go
@@ -5,12 +5,17 @@ import "time"
 type TrackerID int
 
 type Tracker struct {
-	ID                TrackerID     `json:"id" gorm:"primary_key;auto_increment"`
-	Label             string        `json:"label" codec:"-"`
-	LastPoll          time.Time     `json:"last_poll" codec:"-"`
-	LastSleepTime     time.Time     `json:"last_sleep_time" codec:"-"`
-	LastBatteryUpdate time.Time     `json:"last_battery_update" codec:"-"`
-	LastRoom          RoomID        `json:"last_room" codec:"-"`
+	ID TrackerID `json:"id" gorm:"primary_key;auto_increment"`
+	// Label is a human-readable name for the tracker
+	Label string `json:"label" codec:"-"`
+	// LastPoll indicates when the tracker has last polled for a command
+	LastPoll      time.Time `json:"last_poll" codec:"-"`
+	LastSleepTime time.Time `json:"last_sleep_time" codec:"-"`
+	// LastBatteryUpdate indicates when the tracker's battery status has last been updated
+	LastBatteryUpdate time.Time `json:"last_battery_update" codec:"-"`
+	// LastRoom is the last known room in which the tracker was located
+	LastRoom RoomID `json:"last_room" codec:"-"`
+	// Status indicates what mode of operation the tracker is in
 	Status            TrackerStatus `json:"status" codec:"-"`
 	BatteryPercentage int           `json:"battery_percentage" codec:"-"`
 	IsCharging        bool          `json:"is_charging" codec:"-"`
