75ca0c82ae3c9abf0a0000cfeafe7ef61846fe9a
Max Heidinger <mail@max-heidinger.de>
2020-04-11 17:09:27 +0200

    App: Tutorial to step 6

--- app/lib/pages/tutorial_page.dart
+++ app/lib/pages/tutorial_page.dart
@@ -58,17 +58,25 @@ class _TutorialPageState extends State<TutorialPage> {
 }
 
 var tutorialList = <Tutorial>[
-  Tutorial("Welcome", "assets/images/tutorial_01.png", "Swipe to begin with the tutorial!"),
-  Tutorial("Tabs", "assets/images/tutorial_02.png",
-      "The app is divided into 4 different tabs. Each tab provides you with a list of: workflows in execution, workflow templates, rooms and trackers."),
+  Tutorial("Welcome", "Swipe to begin with the tutorial!"),
+  Tutorial("Configuration",
+      "Enter the URL and optionally the port of the server where the backend software is running on. The default port is 8080."),
+  Tutorial("Main Screen",
+      "The app is divided into 4 different tabs. Each tab provides you with a list of: workflows in execution, workflow templates, rooms and trackers. On the top right you find this tutorial."),
+  Tutorial("Tracker List",
+      "Switch to the fourth tab for all trackers. If your list is empty use the Flasher tool to setup your tracker."),
+  Tutorial("Pull to refresh",
+      "Pull down the empty list to refresh. It should the show your set up tracker. This works on all tabs and also detail pages."),
+  Tutorial("Tracker Detail",
+      "Tap on the tracker to open its Detail Page. Tap on the edit button to enable editing and the same button again to save. The delete button deletes the tracker."),
+  Tutorial("Room List", "tutorial_07.png"),
 ];
 
 class Tutorial {
   final String title;
-  final String imageURL;
   final String description;
 
-  Tutorial(this.title, this.imageURL, this.description);
+  Tutorial(this.title, this.description);
 }
 
 class TutorialItem extends StatelessWidget {
@@ -88,7 +96,7 @@ class TutorialItem extends StatelessWidget {
           children: [
             Text(tutorial.title, style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
             Padding(padding: EdgeInsets.only(top: 30.0)),
-            Image(image: AssetImage(tutorial.imageURL)),
+            Image(image: AssetImage("assets/images/tutorial_$index.png")),
             Padding(padding: EdgeInsets.only(top: 30.0)),
             Text(tutorial.description, style: TextStyle(fontSize: 25.0)),
           ],
